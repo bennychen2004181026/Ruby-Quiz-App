cbbcba1401d81c9d54a548cea4d8e57a36c90b17
bennychen2004181026 <ben83026@outlook.com>
2021-09-29 01:48:25 +1000

    working on add quiz collection on custom feature

--- src/models/Custom.rb
+++ src/models/Custom.rb
@@ -6,16 +6,44 @@ class Custom
     def initialize()
         @custom
     end
+
    def custom_load
     @custom=FileManager.load_custom
     return @custom
    end
-   def custom_add(custom_name,question_content,a,b,c,d,right_answer)
-   custom_json=FileManager.load_custom
-   if custom_json==nil
-    puts "Oh, it seeems you get nothing from the custom collection file"
-    
+
+   def add_empty_collection(custom_name)
+    custom_json=FileManager.load_custom
+   new_custom_Id = custom_json["Custom"].map{|h|h["Custom_Id"]}.max + 1
+   
+   new_custom= Hash.new
+   new_custom["Custom_Id"]=new_custom_Id
+   new_custom["Custom_Name"]=custom_name
+   new_custom["Content"]=[]
+   custom_json["Custom"].push(new_custom)
+    return custom_json,new_custom_Id
    end
 
-   end
+   def fill_empty_collection(question_content,a,b,c,d,right_answer,empty_collection,new_custom_Id)
+    custom_json=empty_collection
+
+   content_hash= Hash.new
+   content_hash["Id"]=1
+   content_hash["Question"]=question_content
+   content_hash["A"]=a
+   content_hash["B"]=b
+   content_hash["C"]=c
+   content_hash["D"]=d
+   content_hash["Right_answer"]=right_answer
+   custom_json["Custom"][new_custom_Id-1]["Content"].push(content_hash)
+   return custom_json
+   end 
+
+
+
+
 end
+a = Custom.new.add_empty_collection("ben")
+puts a.class
+
+
--- src/models/FileManager.rb
+++ src/models/FileManager.rb
@@ -1,19 +1,30 @@
 
 module FileManager
     def self.load_history
-        history_record = File.read(File.join(File.dirname(__FILE__), "../history/history.json"))
+        begin history_record = File.read(File.join(File.dirname(__FILE__), "../history/history.json"))
         history_json = JSON.parse(history_record)
+        rescue ArgumentError, TypeError
+        puts "Unable to read data."
         return history_json
+        end
     end
+
     def self.load_custom
-        custom_collection = File.read(File.join(File.dirname(__FILE__), "../quiz_collections/Custom_collection.json"))
+        begin custom_collection = File.read(File.join(File.dirname(__FILE__), "../quiz_collections/Custom_collection.json"))
         custom_record = JSON.parse(custom_collection)
+        rescue ArgumentError, TypeError
+        puts "Unable to read data."
         return custom_record
+        end
     end
+
     def self.load_default
-        default_collection = File.read(File.join(File.dirname(__FILE__), "../quiz_collections/Default_collection.json"))
+        begin default_collection = File.read(File.join(File.dirname(__FILE__), "../quiz_collections/Default_collection.json"))
         default_record = JSON.parse(default_collection)
+        rescue ArgumentError, TypeError
+        puts "Unable to read data."
         return default_record
+        end
     end
 end
 
